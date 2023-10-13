b65c7dec5fcb13c4dc0e73189c9a1c6b97bf08ee
sujan kumal <mailtosujankumal@gmail.com>
2023-10-13 20:12:32 +0545

    nextjs tailwind start

---
+++ src/components/Header.tsx
@@ -0,0 +1,121 @@
+import Image from "next/image";
+import headerImage from '/public/images/header.jpg';
+
+function List(props: {items:any}){
+    return(
+        <ul>
+            {props.items.map((item:string, index:number) =>{
+                return <li 
+                key={index}
+                style={{animationDelay:`${index*0.5}s`}}
+
+                >
+                    {item}
+                </li>
+            })}
+        </ul>
+    )
+}
+
+function Header() {
+    const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+    return (<>
+        <header className="mb-3">
+            <div className="">
+                <Image
+                    src={headerImage} 
+                    alt="Er. Sujan Kumal" />
+            </div>
+            <div className="bg-gray-900 px-2 border-b-4 border-b-teal-600">
+                <div className="md:flex text-white ">    
+                    <div className="bg-teal-600 w-full p-4 inline-block md:w-1/6 float-left px-4 text-center font-semibold">Latest</div>
+                    <div className="w-full">
+                        <List items={items}/>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="top-header-section">
+                <div className="mgs-container">
+                    <div className="top-left-header">
+                        <div className="date-section">
+                            Saturday, July 04, 2020 </div>
+                        <nav id="top-header-navigation" className="top-navigation">
+                        </nav>
+                    </div>
+                    <div className="top-social-wrapper">
+                        <span className="social-link">
+                            <a href="https://www.facebook.com/Er.SujanKumal.03" target="_blank">
+                                <i className="fa fa-facebook"></i>
+                            </a>
+                        </span>
+                        <span className="social-link">
+                            <a href="https://twitter.com/sujan_03_" target="_blank">
+                                <i className="fa fa-twitter"></i>
+                            </a>
+                        </span>
+                        <span className="social-link">
+                            <a href="https://www.instagram.com/sujan_03_/" target="_blank">
+                                <i className="fa fa-instagram"></i>
+                            </a>
+                        </span>
+                        <span className="social-link">
+                            <a href="https://github.com/sujankumal/" target="_blank">
+                                <i className="fa fa-github"></i>
+                            </a>
+                        </span>
+                    </div>
+                </div> 
+            </div>
+
+            <div className="logo-ads-wrapper clearfix">
+                <div className="mgs-container">
+                    <div className="site-branding">
+                        <div className="site-title-wrapper">
+                            <h1 className="site-title"><a href="/" rel="home">Er. Sujan Kumal</a></h1>
+                            <p className="site-description">Do your duty, but do not concern yourself with the results. </p>
+                        </div>
+                    </div>
+                    <div className="header-ads-wrapper google-adsence">
+                    </div>
+                </div>
+            </div>
+
+            <div id="mgs-menu-wrap" className="bottom-header-wrapper clearfix">
+                <div className="mgs-container">
+                    <div className="home-icon"><a href="/" rel="home"> <i className="fa fa-home"> </i> </a></div>
+                    <a href="javascript:void(0)" className="menu-toggle"> <i className="fa fa-navicon"> </i> </a>
+                    <nav id="site-navigation" className="main-navigation">
+                        <div className="menu">
+                            <ul>
+                                <li className="page_item page-item-2"><a href="/ghost/site/about">About</a></li>
+                                <li className="page_item page-item-14"><a href="/ghost/site/articles">Articles</a></li>
+                                <li className="page_item page-item-3"><a href="/ghost/site/privacy-policy">Privacy Policy</a></li>
+                                <li className="page_item page-item-18"><a href="/ghost/site/twitter">Twitter</a></li>
+                                <li className="page_item"><a href="/ghost/site/jokes">Jokes</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div className="header-search-wrapper">
+                        <span className="search-main"><i className="fa fa-search"></i></span>
+                        <div className="search-form-main clearfix">
+                            <form role="search" method="get" className="search-form" action="">
+                                <label>
+                                    <span className="screen-reader-text">Search for:</span>
+                                    <input type="search" className="search-field" placeholder="Search &hellip;" value=""
+                                        name="s" />
+                                </label>
+                                <input type="submit" className="search-submit" value="Search" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </header>
+
+    </>);
+}
+
+export default Header;
---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+function Footer(props:any) {
+    return ( <>
+         <footer id="colophon" className="bg-gray-900 text-white p-4 text-sm pl-16">
+            <div id="bottom-footer" className="">
+                <div className="mgs-container">
+                    <div className="site-info">
+                        <span className="copy-info">&copy; 2023 Er. Sujan Kumal</span>
+                        <span className="sep"> | </span>
+                        Thank you.
+                    </div>
+                    <nav id="footer-navigation" className="sub-footer-navigation">
+                    </nav>
+                </div>
+            </div>
+        </footer>
+    </> );
+}
+
+export default Footer;
