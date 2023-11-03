8b184d3d140bf2b51da5d20d75179dfd06cf17cb
sujan kumal <mailtosujankumal@gmail.com>
2023-11-03 14:55:01 +0545

    oops but why did you move it outside :D

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,6 +1,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../lib/prisma/prisma";
+import prisma from "../../../../lib/prisma/prisma";
 import { Prisma } from "@prisma/client";
 
 export async function GET(request: NextRequest){
