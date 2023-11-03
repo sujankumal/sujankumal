14b64eff5edb9a2363acf0e84302531f9454dda1
sujan kumal <mailtosujankumal@gmail.com>
2023-11-03 15:56:44 +0545

    prisma is angry with me

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,6 +1,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../lib/prisma/prisma";
+import prisma from "../../../../prisma/prisma";
 import { Prisma } from "@prisma/client";
 
 export async function GET(request: NextRequest){
