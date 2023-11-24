cb981b1ef3d1ecc70beeb9f6255f8d6f99deca1c
sujan kumal <mailtosujankumal@gmail.com>
2023-11-24 22:09:43 +0545

    screen size pagebtnnum

--- src/components/Pagination/paginate-list-buttons.tsx
+++ src/components/Pagination/paginate-list-buttons.tsx
@@ -21,34 +21,32 @@ export default function pageListButtons(currentPage: number, numberOfPages: numb
                 dotDotDotAtStartDisplayFlag = true;
             }
         }
-        return (
-            <>
-                {
-                    (shouldDisplayDotDotDotAtEnd || shouldDisplayDotDotDotAtStart)
-                        ? <li>
-                            <button disabled={true} className="flex hover:bg-white hover:text-gray-500 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</button>
-                        </li>
-                        : null
-                }
-                {
-                    (
-                        (currentPage - ((currentPage >= (numberOfPages - paginationMidVal))
-                            ? totalButtonDisplayOnPagination - 1 - (numberOfPages - currentPage)
-                            : paginationMidVal)) <= val
-                        &&
-                        (currentPage + ((currentPage <= (1 + paginationMidVal))
-                            ? totalButtonDisplayOnPagination - 1 - (currentPage - 1)
-                            : paginationMidVal)) >= val) ?
-                        <li key={index}>
-                            <button onClick={() => { (currentPage === val) ? null : setCurrentPage(val) }}
-                                className={`flex w-8 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${(val == currentPage) ? "border-b-teal-600 dark: border-b-teal-600" : ""}`}>
-                                {val}
-                            </button>
-                        </li>
-                        : null
-                }
-            </>
+        let items = [];
+        if(shouldDisplayDotDotDotAtEnd || shouldDisplayDotDotDotAtStart){
+            items.push( 
+                <li key={'a'+index}>
+                    <button disabled={true} className="flex hover:bg-white hover:text-gray-500 items-center justify-center px-1 h-6 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</button>
+                </li>
+            )
+         }
+        items.push(
+            (
+                (currentPage - ((currentPage >= (numberOfPages - paginationMidVal))
+                    ? totalButtonDisplayOnPagination - 1 - (numberOfPages - currentPage)
+                    : paginationMidVal)) <= val
+                &&
+                (currentPage + ((currentPage <= (1 + paginationMidVal))
+                    ? totalButtonDisplayOnPagination - 1 - (currentPage - 1)
+                    : paginationMidVal)) >= val) ?
+                <li key={index}>
+                    <button onClick={() => { (currentPage === val) ? null : setCurrentPage(val) }}
+                        className={`flex w-8 items-center justify-center px-1 h-6 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${(val == currentPage) ? "border-b-teal-600 dark:border-b-teal-600" : ""}`}>
+                        {val}
+                    </button>
+                </li>
+                : null
         )
+        return items;  
     })
 
 }
--- src/components/Pagination/pagnate-post.tsx
+++ src/components/Pagination/pagnate-post.tsx
@@ -7,6 +7,7 @@ import DateTime from "../DateTime/DateTime";
 import UserLinkButton from "../User/UserLinkButton";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import paginateListButtons from "./paginate-list-buttons";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 function PaginationPost(
     { items, pageSize, path }:
@@ -14,10 +15,23 @@ function PaginationPost(
 
     const [posts, setPosts] = useState<Array<PostType>>([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const numberOfPages = Math.floor(items.length / pageSize);
+    
     const numberOfPagesCeil = Math.ceil(items.length / pageSize);
-    const totalButtonDisplayOnPagination = 7;
-
+    const [totalButtonDisplayOnPagination, settotalButtonDisplayOnPagination] = useState(7);
+    
+    const theme = useTheme();
+    const matches_sm = useMediaQuery(theme.breakpoints.down('sm'));
+    const matches_md = useMediaQuery(theme.breakpoints.down('md'));
+    
+    useEffect(()=>{
+        if(matches_sm){
+            settotalButtonDisplayOnPagination(3);
+        }else if(matches_md){
+            settotalButtonDisplayOnPagination(5);
+        }else{
+            settotalButtonDisplayOnPagination(7);
+        }
+    },[matches_sm, matches_md]);
 
     useEffect(() => {
         setPosts(items.slice((currentPage - 1) * pageSize, currentPage * pageSize));
@@ -57,7 +71,7 @@ function PaginationPost(
             
             <nav className="w-full inline-flex justify-center">
                 {/* Navigation For Pagination */}
-                <ul className="flex items-center -space-x-px h-8 text-xs">
+                <ul className="flex items-center h-6 text-xs">
                     <li>
                         <button disabled={currentPage <= 1}
                             onClick={() => {
@@ -65,10 +79,9 @@ function PaginationPost(
                                     setCurrentPage(currentPage - 1);
                                 }
                             }}
-                            className="flex disabled:hover:bg-white disabled:hover:text-gray-300 disabled:text-gray-300 items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            className="flex disabled:hover:bg-white disabled:hover:text-gray-300 disabled:text-gray-300 items-center justify-center px-3 h-6 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                             <span className="sr-only">Previous</span>
                             <ChevronLeft />
-                            <span>Previous</span>
                         </button>
                     </li>
                     {
@@ -80,9 +93,8 @@ function PaginationPost(
                                 setCurrentPage(currentPage + 1);
                             }
                         }} disabled={currentPage >= numberOfPagesCeil}
-                            className="flex disabled:hover:bg-white disabled:hover:text-gray-300 disabled:text-gray-300 items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            className="flex disabled:hover:bg-white disabled:hover:text-gray-300 disabled:text-gray-300 items-center justify-center px-3 h-6 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                             <span className="sr-only">Next</span>
-                            <span>Next</span>
                             <ChevronRight />
                         </button>
                     </li>
