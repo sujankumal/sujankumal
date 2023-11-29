4e58b2895df13b0e972a4abde6a094a554499d4b
sujan kumal <mailtosujankumal@gmail.com>
2023-11-30 00:00:54 +0545

    path url to paginate post

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,6 @@
 import PaginationPost from "@/components/Pagination/pagnate-post";
 import Sidebar from "@/components/Sidebar";
 
-import { APP_BASE_URL } from "@/constants/constants";
 import { fetchArticles } from "@/services/data_access";
 import { PostType } from "@/types/post";
 
@@ -11,7 +10,7 @@ function Articles() {
     return (
         <main className="grid md:grid-cols-4 min-h-screen justify-center">
             <div className="mb-8 p-4 md:m-8 md:col-span-3">
-                <PaginationPost items={articles} pageSize={10} path={APP_BASE_URL+"/articles/"}/>
+                <PaginationPost items={articles} pageSize={10} path={"/articles/"}/>
             </div>
             <aside className="w-full md:col-span-1">
                 <div className="h-full px-3 py-4 overflow-y-auto dark:bg-gray-800">
