ea8e88af82f4da19f250b0c184ae858c0c1d5cb2
Sujan Kumal <mailtosujankumal@gmail.com>
2025-03-23 18:14:15 +0545

    allow /|

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
     },
     sitemap: 'https://sujankumal.com.np/sitemap.xml',
   }
