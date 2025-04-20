e6ecbe9e7c54a3307267924e425d40b7c708a62d
Sujan Kumal <mailtosujankumal@gmail.com>
2025-04-20 22:01:10 +0545

    clear config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        const headers = [];
-        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
-            headers.push({
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'X-Robots-Tag',
-                        value: 'index, follow',
-                    },
-                ],
-            });
-        }
-        return headers;
-    },
 };
 
 module.exports = nextConfig;
