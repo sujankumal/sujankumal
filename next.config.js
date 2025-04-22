3e06cd66521ca2e4f5f762f4912ab7f0b954a3ac
Sujan Kumal <mailtosujankumal@gmail.com>
2025-04-22 23:08:12 +0545

    Revert "clear config"
    This reverts commit e6ecbe9e7c54a3307267924e425d40b7c708a62d.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+            headers.push({
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'index, follow',
+                    },
+                ],
+            });
+        }
+        return headers;
+    },
 };
 
 module.exports = nextConfig;
